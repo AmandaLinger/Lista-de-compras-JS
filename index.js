9ec7d08d7a6db39765336b1e2c1c9f71ba41104e
Amanda Marques <amandamarqueslinger@gmail.com>
2026-04-23 08:10:52 -0300

    exercícios

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function adcItem(){
     let listaInput = document.getElementById('lista-input').value;
-    let itemContainer = document.querySelector('.item-container');
-    let nameItem = document.querySelector('.name-item')
-
+    let itemTemplate = document.querySelector('.item-container');
 
     if(listaInput == '' || listaInput.trim() == ''){
         window.alert("Adicione um novo item por favor")
-    } else{
-        itemContainer.classList.add('displayVisivel')
-        nameItem.textContent = listaInput
+    } else {
+        // Clona o template
+        let novoItem = itemTemplate.cloneNode(true);
 
+        novoItem.classList.remove('displayNone');
+        
+        // Adiciona o nome do item
+        novoItem.querySelector('.name-item').textContent = listaInput;
+        
+        // Insere o novo item após o template
+        itemTemplate.after(novoItem);
+        
+        // Limpa o input
+        document.getElementById('lista-input').value = '';
+        
+        // Adiciona funcionalidade ao botão de deletar
+        novoItem.querySelector('.btn_lixeira').addEventListener('click', function(){
+            let checkboxDoItem = novoItem.querySelector('.radio');
+            
+            if (checkboxDoItem.checked){
+                novoItem.remove();
+            }else{
+                window.alert('Selecione um item para deletar')
+            }
+        })
     }
-
 }
 
 document.querySelector('form').addEventListener('submit', function(){
